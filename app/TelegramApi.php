cac084040bbcf348c6966edb3b10f6f2e54742e6
littleguga <fed777os@gmail.com>
2016-04-26 03:13:42 +0300

    add sendPhoto rename sendMessageAsUrl to sendMessage

--- app/TelegramApi.php
+++ app/TelegramApi.php
@@ -15,7 +15,7 @@ class TelegramApi
      * @param string $message
      * Send message with URL to the post
      */
-    public function sendMessageAsUrl($message)
+    public function sendMessage($message)
     {
         $result = Request::sendMessage(
             [
@@ -23,5 +23,27 @@ class TelegramApi
                 'text' => $message
             ]
         );
+    }
+
+    /**
+     * @param $link
+     * Send photo to channel
+     */
+    public function sendPhoto($link)
+    {
+        //Load file
+        $filename = __DIR__ . DIRECTORY_SEPARATOR . time() . ".jpg";
+        file_put_contents($filename, fopen($link, 'r'));
+
+        //Send file
+        $result = Request::sendPhoto(
+            [
+                'chat_id' => Config::getTelegramChat(),
+            ],
+            $filename
+        );
+
+        //Delete file
+        unlink($filename);
     }
 }
