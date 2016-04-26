cf3f85403743179725ff0f7420881917d9477f31
littleguga <fed777os@gmail.com>
2016-04-26 03:14:20 +0300

    add functions for photo, appendLink, getTextPreview

--- app/VkApi.php
+++ app/VkApi.php
@@ -26,6 +26,25 @@ class VkApi
     }
 
     /**
+     * @param $photo
+     * @return bool|string
+     * Returns url of max sized available photo
+     */
+    public static function findMaxSizeLink($photo)
+    {
+        $prefix = "photo_";
+        $sizes = [2560, 1280, 807, 604, 130, 75];
+
+        foreach ($sizes as $size) {
+            if (isset($photo[$prefix . $size])) {
+                return urldecode($photo[$prefix . $size]);
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * @param $text
      * @return mixed
      * Clears $text from vk links [club1|Test] or [id1|Test]
@@ -51,21 +70,34 @@ class VkApi
 
     /**
      * @param $text
+     * @param $link
+     * @return string
+     * Append "Comment in vk:" to text
+     */
+    public static function appendLink($text, $link)
+    {
+        $text = self::clearVkLinks($text);
+        return trim($text . " Комментировать в ВК:") . " " . $link;
+    }
+
+    /**
+     * @param $text
+     * @param $link
      * @return string
      * Cuts text to 140 symbols or more till the space and add "Read more:" text
      */
-    public static function getTextPreview($text)
+    public static function getTextPreview($text, $link)
     {
         $text = self::clearVkLinks($text);
         $i = 139;
         if (!isset($text[$i])) {
-            return trim($text . " Комментировать в ВК:") . " ";
+            return trim($text . " Комментировать в ВК:") . " " . $link;
         }
         $max = strlen($text) - 1;
         while ($text[$i] != " " and $i < $max) {
             $i++;
         }
         $text = substr($text, 0, $i);
-        return $text . "... Подробнее: ";
+        return $text . "... Подробнее: " . $link;
     }
 }
