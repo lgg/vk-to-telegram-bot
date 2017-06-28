b8032b7ce502e4dfeb03690d2f80acee638f4cc1
littleguga <fed777os@gmail.com>
2017-06-28 19:53:25 +0300

    add year-month-day to logs date

--- app/Log.php
+++ app/Log.php
@@ -1,20 +1,22 @@
 <?php
 
-class Log{
+class Log
+{
     /**
      * @param $str
      * Puts str to log file
      */
     public static function addLog($str)
     {
-        $str = "[" . date('H:i:s') . "]: " . $str . "\n";
+        $str = "[" . date('Y.m.d-H:i:s') . "]: " . $str . "\n";
         file_put_contents(Config::getFileLog(), $str, FILE_APPEND);
     }
 
     /**
      * Clears log file
      */
-    public static function clearLog(){
+    public static function clearLog()
+    {
         unlink(Config::getFileLog());
     }
 
