1d9749bf9f8ed5655102672f8c894d739441d111
littleguga <fed777os@gmail.com>
2018-12-12 12:03:50 +0300

    add 'parse_mode' to allowed parameters

--- app/TelegramApi.php
+++ app/TelegramApi.php
@@ -30,6 +30,7 @@ class TelegramApi
         $allowed = [
             'disable_web_page_preview', // Bool, Disables link previews for links in this message
             'disable_notification', // Bool, Sends the message silently. Users will receive a notification with no sound.
+            'parse_mode', // String 'HTML' or 'Markdown'
         ];
 
         $params = [
